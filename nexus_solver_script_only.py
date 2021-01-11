1060a09b579cde7e55cef41ceeccdcaa7878d051
myrzh <49148712+myrzh@users.noreply.github.com>
2021-01-11 21:28:06 +0500

    More comments

--- nexus_solver_script_only.py
+++ nexus_solver_script_only.py
@@ -11,7 +11,7 @@ def follow(thefile):
             continue
         yield line
 
-if __name__ == "__main__":
+if __name__ == "__main__": # в душе не ебу зачем это нужно
     logfile = open(os.getenv("APPDATA")+"/.minecraft/logs/latest.log", "r")
     loglines = follow(logfile)
     for line in loglines:
